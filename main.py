844bea2974122dd8885575cbd7f13220854c7104
igndev07 <192967484+igndev07@users.noreply.github.com>
2025-01-02 23:20:37 +0530

    added comments

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #pre assuming 2,3 as prime numbers
 #you can edit the range :) 
 
-x = {}
-for i in range(4,10000):  
+x = {} #empty dictionary 
+for i in range(4,10000):  #you can change the limits here
   x[i] = "None"
   y=2
   while y<i:
     if i%y == 0:
-      x[i] = "one"
+      x[i] = "one" #changes made in values for composite number
       y+=1
     else:
       y+=1
@@ -16,5 +16,5 @@ for i in range(4,10000):
 
 print("The prime numbers are:")
 for xx in x:
-  if x[xx] == "None":
+  if x[xx] == "None": #checks for prime number only
     print(xx)
